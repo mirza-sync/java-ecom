79b7385695b9941aad32f3f7c499cb8908698d79
mirza-sync <mirzasyahmi123@gmail.com>
2026-06-24 13:07:59 +0800

    Init config-server

---
+++ services/config-server/src/test/java/com/mirza/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirza.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
