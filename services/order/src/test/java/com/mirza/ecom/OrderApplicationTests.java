3224c5c88530f81daa751ba196fe2d71660ac274
mirza-sync <mirzasyahmi123@gmail.com>
2026-06-30 23:51:50 +0800

    Init order service

---
+++ services/order/src/test/java/com/mirza/ecom/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirza.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
