cb12e5daf8ab1c789ffed9df1c72f7aa5fbfdc9f
mirza-sync <mirzasyahmi123@gmail.com>
2026-07-15 23:53:06 +0800

    Fetch customer in order service from customer service

--- services/order/src/main/java/com/mirza/ecom/order/OrderService.java
+++ services/order/src/main/java/com/mirza/ecom/order/OrderService.java
@@ -1,8 +1,33 @@
 package com.mirza.ecom.order;
 
+import org.springframework.stereotype.Service;
+
+import com.mirza.ecom.customer.CustomerClient;
+import com.mirza.ecom.exception.BusinessException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
 
+    private final CustomerClient customerClient;
+
     public Integer createOrder(OrderRequest request) {
+        var customer = this.customerClient.findCustomerById(request.customerId())
+                .orElseThrow(
+                        () -> new BusinessException("Cannot create order:: No customer exist with the provided id"));
+
+        // purchase product using product microservice
+
+        // persist order
+
+        // persist order lines
+
+        // start payment process
+
+        // send order confirmation to notification microservice (kafka)
+
         return null;
     }
 
